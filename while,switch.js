f4d30ed251bd2d6174f069a97f077e0d746ea8ad
Your Name <you@example.com>
2024-10-03 21:13:01 +0530

    new files are added

---
+++ while,switch.js
@@ -0,0 +1,76 @@
+//Find day of the week using switch case
+var day="Friday"; 
+    switch(day) {
+        case "Monday":
+            console.log("Monday is a working day")
+            break;
+            case "Tuesday":
+                console.log("Tuesday is a working day")
+                break;
+                case "Wednesday":
+            console.log("Wednesday is a working day")
+            break;
+            case "Thursday":
+            console.log("Thursday is a working day")
+            break;
+            case "Friday":
+            console.log("Friday is a working day")
+            break;
+            case "Saturday":
+                console.log("Saturday is a working day")
+                break;
+                case "Sunday":
+                console.log("Sunday is a holiday")
+    }
+    //Find month of the year using switch case
+    var month="May"; 
+    switch(month) {
+        case "January":
+            console.log("January")
+            break;
+            case "February":
+                console.log("February")
+                break;
+                case "March":
+            console.log("March")
+            break;
+            case "April":
+            console.log("April")
+            break;
+            case "May":
+            console.log("May")
+            break;
+            case "June":
+                console.log("June")
+                break;
+                case "July":
+                console.log("July")
+                case "August":
+            console.log("August")
+            break;
+            case "September":
+                console.log("September")
+                break;
+                case "October":
+            console.log("October")
+            break;
+            case "November":
+            console.log("November")
+            break;
+            case "December":
+            console.log("December")
+    }
+    //Find even numbers in first 10 numbers using while loop
+// //using while
+var num1=2;
+while(num1<=10)//limit =10
+    {
+    console.log("num even=",num1)
+num1+=2;}
+//Countdown timer using while loop and the countdown should starts from 10
+let d=10;
+while(d>=0)
+{
+    console.log(d)
+    d--;
+}
